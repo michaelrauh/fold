6a4caa4ac79433cff0dd48588819a8a0403715e1
michaelrauh <michaelrauh@ymail.com>
2025-07-17 20:08:30 -0400

    start fresh

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use clap::Parser;
-use fold::Processor;
-
-#[derive(Parser)]
-struct Args {
-    #[arg(index = 1)]
-    file: String,
-}
-
 fn main() {
-    let args = Args::parse();
-    let processor = Processor::new();
-    processor.process(&args.file);
+    println!("Hello, world!");
 }
